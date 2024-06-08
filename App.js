e5e1d20a7b63b046741528c96f3ac3186fdd467b
Zixon77 <chikaosro@gmail.com>
2024-06-08 19:34:05 -0400

    Set up a bunch of stuff

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./src/screens/Home"
+import Settings from "./src/screens/Settings"
+import About from "./src/screens/About"
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+
+export const onLayoutRootView = async (fontsLoaded, fontError) => {
+  if (fontsLoaded || fontError) {
+    await SplashScreen.hideAsync();
+  }
+};
 
 export default function App() {
+  const Stack = createStackNavigator();
+
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded, fontError] = useFonts({
+    'Lexend-Regular': require('./src/assets/fonts/Lexend-Regular.ttf'),
+    'Lexend-Medium': require('./src/assets/fonts/Lexend-Medium.ttf'),
+    'Lexend-SemiBold': require('./src/assets/fonts/Lexend-SemiBold.ttf'),
+    'Lexend-Bold': require('./src/assets/fonts/Lexend-Bold.ttf'),
+  });
+
+  onLayoutRootView(fontsLoaded, fontError);
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ gestureEnabled: false, headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
